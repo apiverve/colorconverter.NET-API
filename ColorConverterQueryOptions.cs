69d8dba6a43078a9268fc6f5cefea579601f6596
APIVerve Team <hello@apiverve.com>
2024-08-20 10:45:45 -0500

    Bug Fixes

---
+++ ColorConverterQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class ColorConverterQueryOptions {
+public string hex { get; set; }
+}
+
+}
